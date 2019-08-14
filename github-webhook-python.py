b6a7a577fb8582cbac28a5fb5abdc349168c95cb
arvon <guoyafeng@taiyouxi.cn>
2019-08-15 01:20:35 +0800

    改用BaseHttpServer，支持secret

--- github-webhook-python.py
+++ github-webhook-python.py
@@ -1,51 +1,45 @@
-#!/usr/bin/env python
-# Requier: pip install GitPython
-# http://note.qidong.name/2018/01/gitpython/
-import socket
-import threading
-import os
-import git
-import time
+# -*- coding: utf-8 -*-
+import BaseHTTPServer
+import json
+import hmac
+from hashlib import sha1
+from collections import OrderedDict
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-class init_socket():
-    def __init__(self, HOST_NAME, HOST_PORT, MAX_CLIENT):
-        self.host_name = HOST_NAME
-        self.host_port = HOST_PORT
-        self.ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ss.bind((HOST_NAME, HOST_PORT))
-        self.ss.listen(MAX_CLIENT)
-    def run(self, REMOTE_PATH, LOCAL_PATH):
-        res_ok = 'HTTP/1.1 200 OK\nContent-Type:text/html\nServer:myserver\n\nHello, Gourds!'
-        res_xx = 'HTTP/1.1 404 OK\nContent-Type:text/html\nServer:myserver\n\nFriend!'
-        while True:
-            sock, addr = self.ss.accept()
-            #print sock, addr
-            request = sock.recv(1024)
-            print request
-            method = request.split(' ')[0]
-            src = request.split(' ')[1]
-            #print method, src
-            if method == 'POST':
-               if src == '/hook/push':
-                   content = res_ok
-                   if len(os.listdir(LOCAL_PATH)) <= 1:
-                       git.Repo.clone_from(url=REMOTE_PATH, to_path=LOCAL_PATH)
-                       print "Run clone command"
-                   else:
-                       gp = git.cmd.Git(LOCAL_PATH)
-                       gp.pull()
-                       print "Run pull command"
-               else:
-                   content = res_xx
-            else:
-               content = res_xx
-            print content
-            sock.sendall(content)
-            print request
-            sock.close()
-            time.sleep(60)
-
+class my_web(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_POST(self):
+        primary_data = self.rfile.read(int(self.headers['Content-Length']))
+        format_data = json.dumps(json.loads(primary_data,object_pairs_hook=OrderedDict),separators=(',',':'),ensure_ascii=False)
+        secret_check = 'sha1=' + hmac.new('your_secret', msg=format_data, digestmod=sha1).hexdigest()
+        if self.path == '/hook/push/' and  secret_check == self.headers['X-Hub-Signature']:
+            self.send_response(200)
+            message = json.dumps('Success Recive')
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(message)
+            run_command('xxxx','./xxxx/')
+        else:
+            self.send_response(403)
+            message = json.dumps('Error Action')
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(message)
+def run_command(remote_url, local_dir):
+    import git
+    import os
+    if not os.path.exists(local_dir):
+        os.makedirs(local_dir)
+    if len(os.listdir(local_dir)) <= 1:
+        git.Repo.clone_from(url=remote_url, to_path=local_dir)
+        print 'clone success'
+    else:
+        gp = git.cmd.Git(local_dir)
+        gp.pull()
+        print 'pull success'
 
 if __name__ == '__main__':
-    my_app = init_socket("", 18080, 5)
-    my_app.run('xxx', 'xxx')
+    server_address = ('', 8000)
+    my_app= BaseHTTPServer.HTTPServer(server_address=server_address,RequestHandlerClass=my_web,bind_and_activate=True)
+    my_app.serve_forever()
